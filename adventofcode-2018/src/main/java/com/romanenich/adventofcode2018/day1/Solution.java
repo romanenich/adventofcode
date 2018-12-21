2d4f3c40367d71153c393cd42b612b078ac91971
Roman Romanenko <romanenich@gmail.com>
2018-12-21 23:46:32 +0200

    Day1 solution (part two)

--- adventofcode-2018/src/main/java/com/romanenich/adventofcode2018/day1/Solution.java
+++ adventofcode-2018/src/main/java/com/romanenich/adventofcode2018/day1/Solution.java
@@ -6,6 +6,10 @@ import com.romanenich.Utils;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Solution {
 
@@ -13,22 +17,55 @@ public class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.getResult());
+        System.out.println("Resulting frequency is " + solution.getResult().sum);
+        System.out.println("The first frequency reached twice is " + findFirstFrequencyReachedTwice(solution.getResult().numbers));
     }
 
-    private int getResult() {
-        int result = 0;
+    private static int findFirstFrequencyReachedTwice(List<Integer> numbers) {
+        Set<Integer> checks = new TreeSet<>();
+        int size = numbers.size();
+        int curFreq = 0;
+        while (true) {
+            for (Integer number : numbers) {
+                curFreq += number;
+                if (checks.contains(curFreq) || curFreq == 0) {
+                    return curFreq;
+                }
+                checks.add(curFreq);
+            }
+        }
+    }
+
+    private DataAggregator getResult() {
+        DataAggregator retVal = new DataAggregator();
         try (BufferedReader fileReader = new BufferedReader(new FileReader(Utils.getFileByName(getClass(), FILE_PATH)))) {
             String readLine = fileReader.readLine();
             while (readLine != null) {
-                result += Integer.valueOf(readLine);
+                retVal.add(Integer.valueOf(readLine));
                 readLine = fileReader.readLine();
             }
         } catch (IOException | NumberFormatException e) {
             throw new AdventOfCodeException("Something went wrong", e);
         }
-        return result;
+        return retVal;
     }
 
+    private class DataAggregator {
+        private List<Integer> numbers = new ArrayList<>();
+        private Integer sum = 0;
+
+        private void add(Integer number) {
+            sum += number;
+            numbers.add(number);
+        }
+
+        @Override
+        public String toString() {
+            return "DataAggregator{" +
+                    "numbers=" + numbers +
+                    ", sum=" + sum +
+                    '}';
+        }
+    }
 
 }
