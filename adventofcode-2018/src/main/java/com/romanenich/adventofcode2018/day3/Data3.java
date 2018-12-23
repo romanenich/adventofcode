63efa7e6762f3e124a5d02ddb9f4a3e489f1284a
Roman Romanenko <romanenich@gmail.com>
2018-12-23 21:27:48 +0200

    Data3 has been added

---
+++ adventofcode-2018/src/main/java/com/romanenich/adventofcode2018/day3/Data3.java
@@ -0,0 +1,12 @@
+package com.romanenich.adventofcode2018.day3;
+
+/**
+ * Created by Roman on 23.12.2018.
+ */
+public class Data3 {
+    private int id;
+    private int shiftLeft;
+    private int shiftTop;
+    private int width;
+    private int tall;
+}
