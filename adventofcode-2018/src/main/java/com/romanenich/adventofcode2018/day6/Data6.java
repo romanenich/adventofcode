dc194f5e03c7bb0ad0486b9730e1824486f724a6
Roman Romanenko <romanenich@gmail.com>
2019-01-04 21:25:58 +0200

    Day init commit

---
+++ adventofcode-2018/src/main/java/com/romanenich/adventofcode2018/day6/Data6.java
@@ -0,0 +1,7 @@
+package com.romanenich.adventofcode2018.day6;
+
+/**
+ * Created by Roman on 04.01.2019.
+ */
+public class Data6 {
+}
---
+++ adventofcode-2018/src/main/java/com/romanenich/adventofcode2018/day6/DataLoader6.java
@@ -0,0 +1,18 @@
+package com.romanenich.adventofcode2018.day6;
+
+import com.romanenich.AbstractDataLoader;
+
+/**
+ * Created by Roman on 04.01.2019.
+ */
+public class DataLoader6 extends AbstractDataLoader<Data6> {
+
+    public DataLoader6(String filePath) {
+        super(filePath);
+    }
+
+    @Override
+    protected Data6 convert(String line) {
+        return null;
+    }
+}
